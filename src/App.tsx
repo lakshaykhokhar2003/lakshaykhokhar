402316724b6d889f385ca29f6a1296b07022e5c7
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2024-09-27 00:22:46 +0530

    add: added lazy imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,55 @@
-import Navbar from "./sections/Navbar.tsx";
+import {lazy} from "react";
+import StarsBackground from "./components/ui/StarBackground.tsx";
+import ShootingStars from "./components/ui/ShootingStars.tsx";
 import Hero from "./sections/Hero.tsx";
-import About from "./sections/About.tsx";
-import Projects from "./sections/Projects.tsx";
-import Contact from "./sections/Contact.tsx";
-import Footer from "./sections/Footer.tsx";
-import Experience from "./sections/Experience.tsx";
+
+const Navbar = lazy(() => import("./sections/Navbar.tsx"));
+// const Hero = lazy(() => import("./sections/Hero.tsx"));
+const About = lazy(() => import("./sections/About.tsx"));
+const Projects = lazy(() => import("./sections/Projects.tsx"));
+const Contact = lazy(() => import("./sections/Contact.tsx"));
+const Footer = lazy(() => import("./sections/Footer.tsx"));
+const Experience = lazy(() => import("./sections/Experience.tsx"));
+// const BackgroundBeams = lazy(() => import("./components/ui/BackgroundBeams.tsx"));
+// const ShootingStars = lazy(() => import("./components/ui/ShootingStars.tsx"));
+// const StarsBackground = lazy(() => import("./components/ui/StarBackground.tsx"));
+
 
 const App = () => {
+    // const {progress} = useProgress();
+    // const [showLoading, setShowLoading] = useState(true);
+    //
+    // useEffect(() => {
+    //     if (progress === 100) {
+    //         const timer = setTimeout(() => {
+    //             setShowLoading(false);
+    //         }, 5000);
+    //
+    //         return () => clearTimeout(timer);
+    //     }
+    // }, [progress]);
     return (
-        <main className="max-w-7xl mx-auto relative">
-            <Navbar/>
-            <Hero/>
-            <About/>
-            <Projects/>
-            <Experience/>
-            <Contact/>
-            <Footer/>
-        </main>
+        <>
+            {/*<Suspense*/}
+            {/*    fallback={<div className={"w-full h-full absolute inset-0 bg-white"}>sTARS...</div>}>*/}
+            <main className="max-w-7xl mx-auto relative z-1000">
+
+                <Navbar/>
+                <Hero/>
+                <About/>
+                <Experience/>
+                <Projects/>
+                <Contact/>
+                <Footer/>
+                {/*{showLoading  && <Loading/>}*/}
+            </main>
+            <StarsBackground/>
+            <ShootingStars/>
+
+            {/*</Suspense>*/}
+            {/*<BackgroundBeams/>*/}
+
+        </>
     )
 }
 export default App
