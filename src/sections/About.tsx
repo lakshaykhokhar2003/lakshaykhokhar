e20a0fde2c25a1ad5f2e7e621bc73e7e2e0f6a5f
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2025-04-21 21:59:08 +0530

    feat(update): changed icon and image, added my image in about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -21,7 +21,7 @@ const About = () => {
             <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain"/>
+                        <img src="/ghibli.jpg" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain"/>
                         <div>
                             <p className="grid-headtext">Hi, I'm Lakshay</p>
                             <p className="grid-subtext">I am a Frontend Developer and Full-Stack Developer based in
